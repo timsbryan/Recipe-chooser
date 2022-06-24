e43e4b7ad08ca7583d555dc3e874496c8702538d
Tim Bryan <kabatwa@gmail.com>
2022-06-24 20:36:05 +0100

    basic PWA

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,48 @@
+import { manifest, version } from '@parcel/service-worker';
+
+async function install() {
+  const cache = await caches.open(version);
+  await cache.addAll(manifest);
+}
+addEventListener('install', e => e.waitUntil(install()));
+
+async function activate() {
+  const keys = await caches.keys();
+  await Promise.all(
+    keys.map(key => key !== version && caches.delete(key))
+  );
+}
+addEventListener('activate', e => e.waitUntil(activate()));
+
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(version);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
+
+// async function fetch(event) {
+//   event.respondWith(caches.match(event.request)
+//     .then(r => {
+//       if(r) { return r }
+//       const response = fetch(event.request);
+//       const cache = caches.open(version);
+//       cache.put(event.request, response.clone());
+//       return response;
+//     }));
+// }
+
+//   addEventListener('fetch', e => e.waitUntil(fetch(e)));
+
+/*
+* 
 const cacheName = 'sw-v1';
 
 const appShellFiles = [
@@ -66,4 +111,4 @@ self.addEventListener('install', (e) => {
       }))
     }));
   });
-  
+*/
